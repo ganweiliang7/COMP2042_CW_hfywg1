026e45a857baf87f31a7589cc4dad800898da626
ganweiliang7 <hywg1@nottingham.edu.my>
2021-12-06 22:35:39 +0800

    Addded the model class for home menu

---
+++ homeMenu/HomeMenuModel.java
@@ -0,0 +1,26 @@
+package homeMenu;
+
+import main.java.GameFrame;
+
+import java.awt.*;
+
+public class HomeMenuModel extends Component {
+
+
+
+    public Rectangle menuFace;
+    public Rectangle startButton;
+    public  Rectangle menuButton;
+    public  Rectangle highscoreButton;
+    public  Rectangle InstructionButton;
+
+    public GameFrame owner;
+
+    public static boolean startClicked;
+    public static boolean menuClicked;
+    public static boolean highScoreClicked;
+    public static boolean instructionclicked;
+
+
+
+}
